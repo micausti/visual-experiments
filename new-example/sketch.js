397af0fce95e07155330c5cd3dc8c4e79cd104bb
Michelle Austin <michelletoelle@gmail.com>
2021-10-19 19:28:48 +0100

    first commit

---
+++ new-example/sketch.js
@@ -0,0 +1,62 @@
+/// <reference path="../TSDef/p5.global-mode.d.ts" />
+
+var inc = 10;
+var scl = 10;
+var cols, rows;
+
+var zoff = 1;
+
+var fr;
+
+var particles = [];
+
+var flowfield;
+
+function setup() {
+  createCanvas(900, 900);
+  colorMode(HSB, 255);
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  fr = createP('');
+
+  flowfield = new Array(cols * rows);
+
+  for (var i = 0; i < 30; i++) {
+    particles[i] = new Particle();
+  }
+  background(250);
+}
+
+function draw() {
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(15);
+      flowfield[index] = v;
+      xoff += inc;
+      stroke(0, 50);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // strokeWeight(1);
+      // line(0, 0, scl, 0);
+      // pop();
+    }
+    yoff += inc;
+
+    zoff += 0.0005;
+  }
+
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
+
+  fr.html(floor(frameRate()));
+}
